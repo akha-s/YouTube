f772f03b24f098051adb5213423a2c302449a533
akha-s <axadullayevasevinch2@gmail.com>
2025-05-20 22:07:28 +0500

    added active for btns

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
 let videoList = document.querySelector(".vieo");
+let btns = document.querySelectorAll(".btn");
+
+Array.from(btns).forEach((el) => {
+  el.addEventListener("click", () => {
+    Array.from(btns).forEach((btn) => btn.classList.remove("bun"));
+
+    el.classList.add("bun");
+  });
+});
